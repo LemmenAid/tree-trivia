c7a1148618a4c76dea24f095f744ee01bc14a467
Karlan Lemmen <karlanlemmen@gmail.com>
2024-05-27 15:03:23 +0000

    Add basic JavaScript code

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+console.log('test')
+
+let quizForm = document.getElementById('quiz-form');
+let scoreDiv = document.getElementById('score');
+let correctAnswers = ['a', 'd', 'c', 'b', 'a', 'a', 'd', 'd', 'b', 'c'];
+
+quizForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    let userAnswers = [quizForm.q1.value, quizForm.q2.value, quizForm.q3.value, quizForm.q4.value, quizForm.q5.value, quizForm.q6.value, quizForm.q7.value, quizForm.q8.value, quizForm.q9.value, quizForm.q10.value];
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 1;
+        }
+    });
+
+    scoreDiv.innerHTML = `Your score is ${score} / ${correctAnswers.length}.`
+});
